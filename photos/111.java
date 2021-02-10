55c1e00e514ee3669867a8d0a36ba6d116e7639c
zhangchuqi1998 <55055026+zhangchuqi1998@users.noreply.github.com>
2021-02-09 21:20:52 -0500

    Site updated: 2021-02-09 21:20:50

---
+++ photos/111.java
@@ -0,0 +1,12 @@
+ if (Math.abs(x - ((Math.pow(y, number[i]))
+                                * (Math.pow(z, number[j]))
+                                * (Math.pow(q, number[b]))
+                                * (Math.pow(w, number[n])))) < min) {
+                            min = Math.abs(x - ((Math.pow(y, number[i]))
+                                    * (Math.pow(z, number[j]))
+                                    * (Math.pow(q, number[b]))
+                                    * (Math.pow(w, number[n]))));
+                            dd = number[i];
+                            ee = number[j];
+                            cc = number[b];
+                            jj = number[n];
